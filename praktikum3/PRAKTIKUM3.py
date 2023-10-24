d1f6440db96018b5d3146524e86e19a55e37703f
RobertusDaeli1 <148035227+RobertusDaeli1@users.noreply.github.com>
2023-10-24 10:11:27 +0700

    penambahan file

---
+++ praktikum3/PRAKTIKUM3.py
@@ -0,0 +1,14 @@
+import math
+
+# Mengambil input dari pengguna untuk jari-jari lingkaran
+jari_jari = float(input("Masukkan jari-jari lingkaran: "))
+
+# Menghitung luas lingkaran
+luas = math.pi * (jari_jari ** 2)
+
+# Menghitung keliling lingkaran
+keliling = 2 * math.pi * jari_jari
+
+# Menampilkan hasil perhitungan
+print(f"Luas lingkaran: {luas:.2f}")
+print(f"Keliling lingkaran: {keliling:.2f}")
